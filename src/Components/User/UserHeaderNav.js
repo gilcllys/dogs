29dd9b55fc91b7ee035419c30c47ef0940817447
gilcllys <gilcllyscosta@gmail.com>
2022-05-18 21:59:18 -0400

    mobile Menu

--- src/Components/User/UserHeaderNav.js
+++ src/Components/User/UserHeaderNav.js
@@ -1,17 +1,30 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { NavLink, useLocation } from 'react-router-dom'
 import { UserContext } from '../../UserContext'
 import {ReactComponent as MinhasFotos} from '../../Assets/feed.svg'
 import {ReactComponent as Estastistica} from '../../Assets/estatisticas.svg'
 import {ReactComponent as AdicionarFoto} from '../../Assets/adicionar.svg'
 import {ReactComponent as Sair} from '../../Assets/sair.svg'
 import styles from './UserHeaderNav.module.css'
+import useMedia from '../../Hooks/useMedia'
 
 function UserHeaderNav() {
-    const [mobile, setMobile] = React.useState(null)
     const {userLogout} = React.useContext(UserContext)
+    const mobile = useMedia('(max-width:40rem)')
+    const [mobileMenu, setMobileMenu] = React.useState(false)
+    const {pathname} = useLocation()
+    React.useEffect(() =>{
+        setMobileMenu(false)
+    },[pathname])
     return (
-        <nav className={styles.nav}>
+        <>
+        <button
+            aria-label='Menu'
+            className={`${styles.mobileButton} ${mobileMenu && styles.mobileButtonActive}`}
+            onClick={()=> setMobileMenu(!mobileMenu)}>
+            </button>
+        <nav className={`${mobile ? styles.navMobile : styles.nav} 
+        ${mobileMenu && styles.navMobileAcvtive}`}>
             <NavLink to='/conta' end className={({isActive}) => isActive ? styles.active : undefined}>
                 <MinhasFotos/>
                 {mobile && 'Minhas Fotos'} 
@@ -31,6 +44,7 @@ function UserHeaderNav() {
                 {mobile && 'Sair'}   
             </button>
         </nav>
+        </>
   )
 }
 
