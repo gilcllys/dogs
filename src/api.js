8184b21cb7ab07cb6d097ead6be4afa8b9a0e672
gilcllys <gilcllyscosta@gmail.com>
2022-05-24 20:24:07 -0400

    foto delete

--- src/api.js
+++ src/api.js
@@ -97,3 +97,17 @@ export function COMMENT_POST(id, body){
         
     }
 }
+
+
+export function PHOTO_DELETE(id){
+    return{
+        url:`${API_URL}/api/photo/${id}`,
+        option:{
+            method: 'DELETE',
+            headers:{
+                Authorization : 'Bearer' + window.localStorage.getItem('token'),
+            }, 
+        },
+        
+    }
+}
