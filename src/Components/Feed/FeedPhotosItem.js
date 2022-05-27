caccb41f4e45928849d1502ad0ede978011da186
gilcllys <gilcllyscosta@gmail.com>
2022-05-26 20:08:36 -0400

    image skeleton

--- src/Components/Feed/FeedPhotosItem.js
+++ src/Components/Feed/FeedPhotosItem.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Image from '../Helper/Image';
 import styles from './FeedPhotosItem.module.css'
 
 function FeedPhotosItem({photo,setModalPhoto}) {
@@ -7,8 +8,8 @@ function FeedPhotosItem({photo,setModalPhoto}) {
   }
   return (
     <li className={styles.photo} onClick={handleClick}>
-        <img src={photo.src} alt={photo.title} />
-        <span className={styles.visualizacao}>{photo.acessos}</span>
+      <Image src={photo.src} alt={photo.title} />  
+      <span className={styles.visualizacao}>{photo.acessos}</span>
     </li>
   )
 }
