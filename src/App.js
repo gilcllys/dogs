f7172c45b3693638be028fbc14cd44976b28cc12
gilcllys <gilcllyscosta@gmail.com>
2022-06-04 17:04:27 -0400

    footer

--- src/App.js
+++ src/App.js
@@ -7,29 +7,42 @@ import ProtectedRoute from "./Components/Helper/ProtectedRoute";
 import Home from "./Components/Home";
 import Login from "./Components/Login/Login";
 import User from "./Components/User/User";
-import Photo from './Components/Photo/Photo'
-import { UserStorage } from './UserContext';
+import Photo from "./Components/Photo/Photo";
+import { UserStorage } from "./UserContext";
 import { UserProfile } from "./Components/User/UserProfile";
 import NotFound from "./Components/NotFound";
 
 function App() {
-
-  return (
-      <BrowserRouter>
-        <UserStorage>
-          <Header />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="Login/*" element={<Login />} /> 
-            <Route path="conta/*" element={<ProtectedRoute> <User /> </ProtectedRoute>}/>    
-            <Route path="foto/:id" element={<Photo />} /> 
-            <Route path="perfil/:user" element={<UserProfile />} /> 
-            <Route path="*" element={<NotFound />} /> 
-          </Routes>
-          <Footer />
-        </UserStorage>
-      </BrowserRouter>
-  );
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <UserStorage>
+                    <Header />
+                    <main className="AppBody">
+                        <Routes>
+                            <Route path="/" element={<Home />} />
+                            <Route path="Login/*" element={<Login />} />
+                            <Route
+                                path="conta/*"
+                                element={
+                                    <ProtectedRoute>
+                                        <User />
+                                    </ProtectedRoute>
+                                }
+                            />
+                            <Route path="foto/:id" element={<Photo />} />
+                            <Route
+                                path="perfil/:user"
+                                element={<UserProfile />}
+                            />
+                            <Route path="*" element={<NotFound />} />
+                        </Routes>
+                    </main>
+                    <Footer />
+                </UserStorage>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
